7c3f0878db59ec1e0bb4036d0b816790b854057f
Tiago <lazy.ufms@gmail.com>
2021-09-08 12:17:13 -0400

    v0 sem CSS

---
+++ js/app.js
@@ -0,0 +1,20 @@
+const searchInput = document.querySelector('.searchInput')
+const searchBtn = document.querySelector('.searchBtn')
+
+searchBtn.addEventListener('click', () => {
+  request()
+})
+
+const request = async () => {
+  try {
+    const username = searchInput.value
+    const response = await axios.get(`https://api.github.com/users/${username}`)
+    showSearch(response)
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+const showSearch = (data) => {
+  console.log(data)
+}
